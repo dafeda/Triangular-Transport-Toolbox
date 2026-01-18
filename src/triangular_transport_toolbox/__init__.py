c54f61d561c9ed45dc7590f09231dec150d971f7
Feda Curic <feda.curic@gmail.com>
2026-01-18 20:04:58 +0100

    Move rectifier to separate file

--- src/triangular_transport_toolbox/__init__.py
+++ src/triangular_transport_toolbox/__init__.py
@@ -4,7 +4,8 @@ Triangular Transport Toolbox
 A Python package for triangular transport maps in probabilistic modeling.
 """
 
+from triangular_transport_toolbox.rectifier import Rectifier
 from triangular_transport_toolbox.transport_map import transport_map
 
 __version__ = "1.0.0"
-__all__ = ["transport_map"]
+__all__ = ["transport_map", "Rectifier"]
